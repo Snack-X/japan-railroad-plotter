77ecd61cd0246014bce6ffbc76f12e7fe4aacd0d
Snack <korsnack@korsnack.kr>
2018-04-11 22:25:18 +0900

    Add data first

---
+++ README.md
@@ -0,0 +1,34 @@
+# Japan Railroad Plotter
+
+## Data
+
+Source data and converted data are quite huge and not included on this repository. You should download source data by yourself and place them under `data_raw` directory.
+
+* http://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-N05-v1_3.html
+
+`data_raw` directory tree should look like this:
+
+```
+data_raw/
+    japan-railway/
+        KS-META-N05-16.xml
+        N05-16.xml
+        N05-16_RailroadSection2.dbf
+        N05-16_RailroadSection2.geojson
+        N05-16_RailroadSection2.prj
+        N05-16_RailroadSection2.shp
+        N05-16_RailroadSection2.shx
+        N05-16_Station2.dbf
+        N05-16_Station2.geojson
+        N05-16_Station2.prj
+        N05-16_Station2.shp
+        N05-16_Station2.shx
+```
+
+After running `npm install` and `build-data.js` scripts, `data` directory will contain coverted smaller data.
+
+For more information, please refer to the website.
+
+* http://nlftp.mlit.go.jp/ksj/
+
+*All rights reserved. Copyright (c) 1974-2017 National Land Information Division, National Spatial Planning and Regional Policy Bureau, MLIT of Japan.*
---
+++ build-data.js
@@ -0,0 +1,39 @@
+const mapshaper = require("mapshaper");
+mapshaper.enableLogging();
+
+const runMapshaper = commands => new Promise((resolve, reject) => {
+  mapshaper.runCommands(commands, err => {
+    if(err) reject(err);
+    else resolve();
+  });
+});
+
+(async function() {
+
+  // Build Japan railroad data
+  await runMapshaper(
+   `-i data_raw/japan-railway/N05-16_RailroadSection2.shp encoding=shiftjis \
+    -rename-fields 'type=N05_001,lineName=N05_002,company=N05_003,openYear=N05_004,startYear=N05_005b,endYear=N05_005e,groupId=N05_006' \
+    -drop fields=N05_007,N05_008,N05_009,N05_010 \
+    -each 'type = parseInt(type),
+           openYear = parseInt(openYear),
+           startYear = parseInt(startYear),
+           endYear = parseInt(endYear)' \
+    -filter 'endYear == 9999' \
+    -rename-layers railroads \
+    -o data/japan-railway-railroad.json format=topojson precision=0.00001`);
+
+  await runMapshaper(
+   `-i data_raw/japan-railway/N05-16_Station2.shp encoding=shiftjis \
+    -rename-fields 'type=N05_001,lineName=N05_002,company=N05_003,openYear=N05_004,startYear=N05_005b,endYear=N05_005e,groupId=N05_006,stationName=N05_011' \
+    -drop fields=N05_007,N05_008,N05_009,N05_010 \
+    -each 'type = parseInt(type),
+           openYear = parseInt(openYear),
+           startYear = parseInt(startYear),
+           endYear = parseInt(endYear)' \
+    -filter 'endYear == 9999' \
+    -rename-layers stations \
+    -o data/japan-railway-station.json format=topojson precision=0.00001`);
+})()
+  .then(() => { process.exit(0); })
+  .catch(err => { console.error(err.stack || err); process.exit(1); });
