e3f5747eedf92da13ae19069e82920ab56f733e7
Snack <korsnack@korsnack.kr>
2018-08-27 00:13:22 +0900

    More aliases

--- scripts/series-helper.js
+++ scripts/series-helper.js
@@ -6,9 +6,11 @@ const JRP = new JRPlotter(topoRailroad, topoStation);
 // console.log(app.plots.map(({ properties }) => `[ ${properties.lineHash}, ${properties.startHash}, ${properties.endHash} ], // ${properties.lineName} ${properties.startName} → ${properties.endName}`).join('\n'))
 
 const routes = [
+  [ 3282245463, 1236888564, 1953892116 ], // 南海本線 難波 → 泉佐野
+  [ 2250351118, 3511915283, 1875608977 ], // 空港線 泉佐野 → 関西空港
 ];
 
-const targets = ''.split(/[\s駅\-]+/);
+const targets = '難波駅 - 新今宮駅 - 天下茶屋駅 - 堺駅 - 岸和田駅 - 泉佐野駅 - りんくうタウン駅 - 関西空港駅'.split(/[\s駅\-]+/);
 
 const stations = [].concat(...routes.map(r => JRP.getStationsFromLine(r[0])));
 for (const target of targets) {
