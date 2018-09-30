fd67d2d8133f0679a875d4455f13238f7d2f321f
Snack <korsnack@korsnack.kr>
2018-09-30 17:14:32 +0900

    v2.0.5

--- scripts/series-helper.js
+++ scripts/series-helper.js
@@ -6,9 +6,11 @@ const JRP = new JRPlotter(topoRailroad, topoStation);
 // console.log(app.plots.map(({ properties }) => `[ ${properties.lineHash}, ${properties.startHash}, ${properties.endHash} ], // ${properties.lineName} ${properties.startName} → ${properties.endName}`).join('\n'))
 
 const routes = [
+  [ 2607508661,  183437565, 1358614613 ], // 鹿児島線 熊本 → 八代
+  [ 1657494880, 2787597531,  246708082 ], // 肥薩線 八代 → 吉松
 ];
 
-const targets = ''.split(/[\s駅\-・（）]+/).filter(s => s !== '');
+const targets = '熊本駅 - 新八代駅 - 八代駅 - 坂本駅 - 一勝地駅 - 渡駅 - 人吉駅 - 大畑駅 - 矢岳駅 - 真幸駅 - 吉松駅'.split(/[\s駅\-・→（）]+/).filter(s => s !== '');
 
 const stations = [].concat(...routes.map(r => JRP.getStationsFromLine(r[0])));
 for (const target of targets) {
